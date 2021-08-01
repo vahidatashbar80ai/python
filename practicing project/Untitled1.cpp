cbd5169788ca1d2b7389e2e8f59a07e9a383c0bc
vahidatashbar80ai <88287408+vahidatashbar80ai@users.noreply.github.com>
2021-08-01 23:27:24 +0430

    Add files via upload

---
+++ practicing project/Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // for input and ouput command
+#include <stdio.h>
+//#include <conio.h>  // for getch 
+
+using namespace std;
+
+int main(){
+	
+	cout << "hello world" ;
+	float x ;
+	float a,b ;
+	cout<<"please type your equation" ;
+//	scanf("%fx+%f=0",&a,&b);
+//	getch();	
+return 0;
+}
